ba1b34db4bcaa13c257bdcaf626e09aec65542a3
= <=>
2022-10-14 13:15:17 -0300

    finish 2.0

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -103,19 +103,32 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
         paths: pokemon151.map (id => ({
             params: {id}
         })),
-        fallback: false,
+        fallback: 'blocking', 
     }
 }
 
 
-export const getStaticProps: GetStaticProps = async ({params}) => {
-    const {id} = params as {id: string};
-
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  
+    const { id } = params as { id: string };
+  
+    const pokemon = await getPokemonInfo( id );
+  
+    if ( !pokemon ) {
       return {
-        props: {
-          pokemon: await getPokemonInfo( id )
+        redirect: {
+          destination: '/',
+          permanent: false
         }
       }
     }
+  
+    return {
+      props: {
+        pokemon
+      },
+      revalidate: 86400, // 60 * 60 * 24,
+    }
+  }
 
 export default PokemonPage;
